8a0b1c0d25be0fa9bef3656a891bf9b85fb3a150
n3u2o <n3u2o@pm.me>
2021-07-20 03:31:49 +0200

    add mouse class

--- engine/Window.h
+++ engine/Window.h
@@ -2,6 +2,7 @@
 #include "ConfigureWindows.h"
 #include "EngineException.h"
 #include "Keyboard.h"
+#include "Mouse.h"
 
 class Window
 {
@@ -45,6 +46,7 @@ private:
 	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
 public:
 	Keyboard kbd;
+	Mouse mouse;
 private:
 	int width;
 	int height;
