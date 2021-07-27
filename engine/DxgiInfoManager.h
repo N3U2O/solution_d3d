7a2e0b1f8610706f70897c277b504e19fbbd3651
n3u2o <n3u2o@pm.me>
2021-07-28 00:41:51 +0200

    com bs

--- engine/DxgiInfoManager.h
+++ engine/DxgiInfoManager.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "ConfigureWindows.h"
+#include <wrl.h>
+#include <dxgidebug.h>
 #include <vector>
 #include <string>
 
@@ -7,13 +9,13 @@ class DxgiInfoManager
 {
 public:
 	DxgiInfoManager();
-	~DxgiInfoManager();
+	~DxgiInfoManager() = default ;
 	DxgiInfoManager(const DxgiInfoManager&) = delete;
 	DxgiInfoManager& operator=(const DxgiInfoManager&) = delete;
 	void Set() noexcept;
 	std::vector<std::string> GetMessages() const;
 private:
 	unsigned long long next = 0u;
-	struct IDXGIInfoQueue* pDxgiInfoQueue = nullptr;
+	Microsoft::WRL::ComPtr<IDXGIInfoQueue> pDxgiInfoQueue = nullptr;
 };
 
