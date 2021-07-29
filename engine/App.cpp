1b2507dca31cfd043647e2228878ead439579227
n3u2o <n3u2o@pm.me>
2021-07-29 11:44:43 +0200

    rotation matrix

--- engine/App.cpp
+++ engine/App.cpp
@@ -2,15 +2,15 @@
 
 App::App()
 	:
-	wnd( 1024,576,"n3u2o^D3D" )
+	wnd(1024, 576, "n3u2o^D3D")
 {}
 
 int App::Go()
 {
-	while( true )
+	while (true)
 	{
 		// process all messages pending, but to not block for new messages
-		if( const auto ecode = Window::ProcessMessages() )
+		if (const auto ecode = Window::ProcessMessages())
 		{
 			// if return optional has value, means we're quitting so return exit code
 			return *ecode;
@@ -21,8 +21,9 @@ int App::Go()
 
 void App::DoFrame()
 {
-	const float c = sin( timer.Peek() ) / 2.0f + 0.5f;
-	wnd.Gfx().ClearBuffer( c,c,1.0f );
-	wnd.Gfx().DrawTestTriangle();
+	const float t = timer.Peek();
+	const float c = sin(t) / 2.0f + 0.5f;
+	wnd.Gfx().ClearBuffer(c, c, 1.0f);
+	wnd.Gfx().DrawTestTriangle(t);
 	wnd.Gfx().EndFrame();
 }
