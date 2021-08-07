9efd128c341d4a13ca0164b8ce8064a2cb1409fc
n3u2o <n3u2o@pm.me>
2021-08-07 03:45:36 +0200

    CUBE!!!

--- engine/Graphics.cpp
+++ engine/Graphics.cpp
@@ -110,6 +110,7 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 		{
 			float x;
 			float y;
+			float z;
 		} pos;
 		struct
 		{
@@ -120,15 +121,18 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 		} color;
 	};
 
-	// create vertex buffer (1 2d triangle at center of screen)
+	// create vertex buffer (3d cube)
+	float h = 1.0f;//half side length
 	Vertex vertices[] =
-	{//   xxxxx,yyyyy, rrr,ggg,bbb,aaa
-		{  0.0f, 0.5f, 255,  0,  0,  0 },
-		{  0.5f,-0.5f,   0,255,  0,  0 },
-		{ -0.5f,-0.5f,   0,  0,255,  0 },
-		{ -0.3f, 0.3f,   0,255,  0,  0 },
-		{  0.3f, 0.3f,   0,  0,255,  0 },
-		{  0.0f,-0.8f, 255,  0,  0,  0 },
+	{//   ?x,?y,?z,   rrr,ggg,bbb
+		{ -h,-h,-h,   255,  0,  0,255 },
+		{  h,-h,-h,     0,255,  0,255 },
+		{ -h, h,-h,     0,  0,255,255 },
+		{  h, h,-h,   255,255,  0,255 },
+		{ -h,-h, h,   255,  0,255,255 },
+		{  h,-h, h,   255,  0,255,255 },
+		{ -h, h, h,     0, 88,  0,255 },
+		{  h, h, h,   255,255,255,255 },
 	};
 	vertices[0].color.g = 255;
 	wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
@@ -152,10 +156,12 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 	// create index buffer
 	const unsigned short indices[] =
 	{
-		0,1,2,
-		0,2,3,
-		0,4,1,
-		2,1,5,
+		0,2,1, 2,3,1,
+		1,3,5, 3,7,5,
+		2,6,3, 3,6,7,
+		4,5,7, 4,7,6,
+		0,4,2, 2,4,6,
+		0,1,4, 1,5,4,
 	};
 	wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
 	D3D11_BUFFER_DESC ibd = {};
@@ -183,8 +189,9 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 		{
 			dx::XMMatrixTranspose(
 				dx::XMMatrixRotationZ(angle) *
-				dx::XMMatrixScaling(3.0f / 4.0f,1.0f,1.0f) *
-				dx::XMMatrixTranslation(x, y, 0.0f)
+				dx::XMMatrixRotationX(angle) *
+				dx::XMMatrixTranslation(x, y, 4.0f) *
+				dx::XMMatrixPerspectiveLH(1.0f,3.0f/4.0f,0.5f,10.0f)
 			)
 		}
 	};
@@ -226,7 +233,7 @@ void Graphics::DrawTestTriangle(float angle, float x, float y)
 	wrl::ComPtr<ID3D11InputLayout> pInputLayout;
 	const D3D11_INPUT_ELEMENT_DESC ied[] =
 	{
-		{ "Position",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
+		{ "Position",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
 		{ "Color",0,DXGI_FORMAT_R8G8B8A8_UNORM,0,8u,D3D11_INPUT_PER_VERTEX_DATA,0 },
 	};
 	GFX_THROW_INFO(pDevice->CreateInputLayout(
--- engine/VertexShader.hlsl
+++ engine/VertexShader.hlsl
@@ -9,10 +9,10 @@ cbuffer CBuf
 	matrix transform;
 };
 
-VSOut main(float2 pos : Position, float3 color : Color)
+VSOut main(float3 pos : Position, float3 color : Color)
 {
 	VSOut vso;
-	vso.pos = mul(float4(pos.x, pos.y, 0.0f, 1.0f), transform);
+	vso.pos = mul(float4(pos.x, pos.y, pos.z, 1.0f), transform);
 	vso.color = color;
 	return vso;
 }
